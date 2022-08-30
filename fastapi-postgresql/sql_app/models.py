354dfcf1096289130203d1d82ce905c14cf55333
April Yao <scysy2@nottingham.ac.uk>
2022-08-30 23:33:39 +0100

    change parts of models to employee-manager-department

--- fastapi-postgresql/sql_app/models.py
+++ fastapi-postgresql/sql_app/models.py
@@ -5,26 +5,40 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
-
+# TODO: change the models to employee and manager
 #Create SQLALchemy models from the Base class
-class User(Base):       # models
-    __tablename__ = "users"     #table name
+class Employee(Base):       # models
+    __tablename__ = "employee"     #table name
 
     #attributes -> a column in table
     id = Column(Integer, primary_key = True, index = True)
-    email = Column(String, unique = True, index = True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default = True)
+    name = Column(String, nullable = False)
+    salary = Column(Integer)
+    dept_id = Column(Integer, ForeignKey("users.id", onupdate="CASCADE", ondelete="SET NULL"))
+    manager_id = Column(Integer, ForeignKey("users.id", onupdate="CASCADE", ondelete="SET NULL"))
 
     #relationship -> contain the values from other tables related to this one
-    items = relationship("Item", back_populates = "owner")
+    # TODO: rewrite the relationships
+    belong_department = relationship("Item", back_populates = "owner")
+    with_manager = relationship("Item", back_populates = "owner")
 
-class Item(Base):       #models
-    __tablename__ = "items"
+
+class Manager(Base):       #models
+    __tablename__ = "manager"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    name = Column(String, nullable = False)
+    dept_id = Column(Integer, ForeignKey("users.id", onupdate="CASCADE", ondelete="SET NULL"))
+  
+    belong_department = relationship("User", back_populates="items")
+    with_employee = relationship("User", back_populates="items")
 
-    owner = relationship("User", back_populates="items")
+
+class Department(Base):       #models
+    __tablename__ = "department"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable = False)
+   
+    has_manager = relationship("User", back_populates="items")
+    has_employee = relationship("User", back_populates="items")
--- fastapi-postgresql/sql_app/readme.md
+++ fastapi-postgresql/sql_app/readme.md
@@ -1,3 +1,4 @@
+# README part
 ## Run the code
 **Option 1**:
 cd to folder `fastapi-postgresql`, then run `uvicorn sql_app.main:app --reload`
@@ -8,15 +9,30 @@ Where `main` is the file name(`sql_app.`with src), `app` is the name of app vari
 
 ## Swagger UI and ReDoc
 Advantages of FastAPI is it provides interactive API documentations.
-- Add `/docs` at hte end of route, can go to the API provided by *Swagger UI*: [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs)
+- Add `/docs` at hte end of route, can go to the API provided by *Swagger UI*: [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs)<br>
     Every routes will be list out and can be used for easily debugging and testing. Click the available route, then click "**Try it out**" and then "**Execute**".
 - Add `/redoc` at hte end of route, can go to the API provided by *ReDoc*: [http://127.0.0.1:8000/redoc](http://127.0.0.1:8000/redoc)
+<br> 
+---
 
+# Notes part
 ## CRUD and Path Operations
 - `Post` -> `C`: to create/add data.
 - `Get` -> `R`: to read data.
 - `Put` -> `U`: to update data.
 - `Delete` -> `D`: to delete data.
+
+## Relationship Patterns in SQLAlchemy
+|   |O - O|O - M|M - O|M - M|
+|:--|---|---|---|---|
+|**ForeignKey() on**|a|child|c|d|
+|**relationship() on**|a|parent|c|d|
+|**Bidirectional**|a|relationship() and `back_populates` on both|c|d|
+||a|OR relationship() and `backref` on only child or parent|c|d|
+
+- `back_populates`: Informs each relationship about the other, so that they are kept in sync. MUST explicitly create the relationships on **both** parent and child classes.
+- `backref`: Shortcut for configuring both parent.children and child.parent relationships at **one place only** on the parent or the child class (not both).
+
 
 ### others
 in post option 3, using docs, you will see `"string"` in the initial request body, and you have to specify the data structure in request body as you wish, like `{"user" : "test user in body"}`. Notice that when specified, use **" " double quotes** and not single quotes to ensure the string is able to be parsed on the receiving end.
