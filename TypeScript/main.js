9f3b4abed71f1ffe6d9f34230b90e4a33a59bc7f
April Yao <yaoapril52@gmail.com>
2022-09-09 15:52:45 +0100

    refactoring TS code, add dropdown menu but need fix style

--- TypeScript/main.js
+++ TypeScript/main.js
@@ -3,22 +3,38 @@ var form = document.querySelector("#new-employee-form"); // selects the first in
 var input = document.querySelector("#new-employee-input");
 var list_element = document.querySelector("#employees");
 var header = document.querySelector("header");
-var submit_button = document.querySelector("#new-employee-submit");
-var todoList = document.querySelector(".employee");
+var dropdowns = document.querySelectorAll(".dropdown");
 // ----- Event Listeners -----
-// submit_button.addEventListener('click', addName);
-if (!form) {
-    console.log("cannot found form");
-}
-else {
+if (form) {
     form.addEventListener("submit", addNew);
 }
-if (!list_element) {
-    console.log("cannot found list element");
+if (list_element) {
+    list_element.addEventListener("click", editNDelete);
 }
-else {
-    list_element.addEventListener("click", check);
-}
+dropdowns.forEach(function (dropdown) {
+    var select = dropdown.querySelector(".select");
+    var icon = dropdown.querySelector(".icon");
+    var menu = dropdown.querySelector(".menu");
+    var options = dropdown.querySelectorAll(".menu li");
+    var selected = dropdown.querySelector(".selected");
+    select.addEventListener("click", function () {
+        select.classList.toggle("select-clicked"); //add clicked select style to the select element
+        icon.classList.toggle("icon-rotate"); // add rotate style to icon
+        menu.classList.toggle("menu-open"); // add open style to menu
+    });
+    options.forEach(function (option) {
+        option.addEventListener("click", function () {
+            selected.innerText = option.innerText; // change the inner text to clicked option's inner text
+            select.classList.remove("select-clicked"); // remove the clicked select style
+            select.classList.remove("icon-rotate"); // remove rotate style to icon
+            menu.classList.remove("menu-open"); // remove open style to the menu
+            options.forEach(function (option) {
+                option.classList.remove("active"); // remove active class from all options
+            });
+            option.classList.add("active"); // add active style to this clicked option
+        });
+    });
+});
 // ----- Functions -----
 // create element and add to class
 function createElWithClass(newEl, newClass) {
@@ -64,123 +80,113 @@ function addNew(e) {
         console.log("cannot found list element");
         return;
     }
-    list_element.appendChild(employee_element);
+    else {
+        list_element.appendChild(employee_element);
+    }
     input.value = ""; // clean the value after append
-    employee_edit.addEventListener("click", function () {
+}
+// edit employee name or delete the employee
+function editNDelete(e) {
+    console.log(e.target);
+    var item = e.target;
+    var employee_action = item.parentElement;
+    var employee_element = employee_action.parentElement;
+    if (item.classList[0] === "delete") {
+        if (confirm("Are you sure to kick this person out?")) {
+            employee_element.classList.toggle("employee-fall"); // add animation
+            employee_element.addEventListener("transitionend", function () {
+                employee_element.remove(); // remove after transition end
+            });
+        }
+    }
+    if (item.classList[0] === "edit") {
+        var employee_input = employee_element.querySelector(".text");
         if (employee_input.readOnly) {
             employee_input.removeAttribute("readonly");
             employee_input.focus();
-            employee_edit.innerText = "Save";
+            item.innerText = "Save";
         }
         else {
             console.log("Save");
             employee_input.setAttribute("readonly", "readonly");
-            employee_edit.innerText = "Edit";
+            item.innerText = "Edit";
         }
-    });
-    employee_delete.addEventListener("click", function () {
-        if (confirm("Are you sure to kick this person out?")) {
-            list_element.removeChild(employee_element);
-        }
-    });
+    }
 }
-// function addName(event:Event) {
-//     event.preventDefault();
-//     // console.log("click");
-//     const todo_div = createElWithClass("div", "employee");
-//     const new_todo = createElWithClass("li", "content");
-//     new_todo.innerText = 'hey';
-//     todo_div.appendChild(new_todo);
-//     const employee_edit = createElWithClass("button", "edit");
-//     employee_edit.innerHTML = "Edit";
-//     const employee_delete = createElWithClass("button", "delete");
-//     employee_delete.innerHTML = "Delete";
-//     todo_div.appendChild(employee_edit);
-//     todo_div.appendChild(employee_delete);
-//     if (!list_element) {
-//         console.log("cannot found list element");
+// window.addEventListener("load", () => {
+//     if (!form) {
+//         console.log("cannot found form");
 //         return;
 //     }
-//     list_element.appendChild(todo_div);
-// }
-function check(e) {
-    console.log(e.target);
-}
-window.addEventListener("load", function () {
-    if (!form) {
-        console.log("cannot found form");
-        return;
-    }
-    form.addEventListener("submit", function (e) {
-        e.preventDefault(); // stop from refresh the page
-        // console.log("submit success");
-        if (!input) {
-            console.log("cannot found input");
-            // alert("please enter something ");
-            return;
-        }
-        var employee = input.value;
-        // console.log("you entered: " + employee);
-        if (!header) {
-            console.log("cannot found header");
-            return;
-        }
-        if (!employee) {
-            var incorrectSub_2 = document.createElement("p");
-            incorrectSub_2.textContent = "Please insert the employee name";
-            incorrectSub_2.style.color = "#E74C3C";
-            header.appendChild(incorrectSub_2);
-            // disappear after 2s
-            setTimeout(function () {
-                header.removeChild(incorrectSub_2);
-            }, 2000);
-            return;
-        }
-        // if (employee){
-        //     console.log("you entered: " + employee);
-        // } else {
-        //     console.log("Nothing entered!");
-        //     alert("please enter something ");
-        //     return;
-        // }
-        var employee_element = createElWithClass("div", "employee");
-        var employee_content = createElWithClass("div", "content");
-        var employee_input = createElWithClass("input", "text");
-        employee_input.type = "text";
-        employee_input.value = employee;
-        employee_input.setAttribute("readonly", "readonly");
-        var employee_action = createElWithClass("div", "actions");
-        var employee_edit = createElWithClass("button", "edit");
-        employee_edit.innerHTML = "Edit";
-        var employee_delete = createElWithClass("button", "delete");
-        employee_delete.innerHTML = "Delete";
-        employee_content.appendChild(employee_input);
-        employee_action.appendChild(employee_edit);
-        employee_action.appendChild(employee_delete);
-        employee_element.appendChild(employee_content);
-        employee_element.appendChild(employee_action);
-        if (!list_element) {
-            console.log("cannot found list element");
-            return;
-        }
-        list_element.appendChild(employee_element);
-        input.value = ""; // clean the value after append
-        employee_edit.addEventListener("click", function () {
-            if (employee_input.readOnly) {
-                employee_input.removeAttribute("readonly");
-                employee_input.focus();
-                employee_edit.innerText = "Save";
-            }
-            else {
-                console.log("Save");
-                employee_input.setAttribute("readonly", "readonly");
-                employee_edit.innerText = "Edit";
-            }
-        });
-        employee_delete.addEventListener("click", function () {
-            if (confirm("Are you sure to kick this person out?")) {
-                list_element.removeChild(employee_element);
-            }
-        });
-    });
-});
+//     form.addEventListener("submit", (e) => {
+//         e.preventDefault();         // stop from refresh the page
+//         // console.log("submit success");
+//         if (!input) {
+//             console.log("cannot found input");
+//             // alert("please enter something ");
+//             return;
+//         }
+//         const employee : string | null = input.value;
+//         // console.log("you entered: " + employee);
+//         if (!header) {
+//             console.log("cannot found header");
+//             return;
+//         }
+//         // if (!employee) {
+//         //     const incorrectSub = document.createElement("p")
+//         //     incorrectSub.textContent = "Please insert the employee name"
+//         //     incorrectSub.style.color = "#E74C3C";
+//         //     header.appendChild(incorrectSub);
+//         //     // disappear after 2s
+//         //     setTimeout(function(){
+//         //        header.removeChild(incorrectSub);
+//         //     }, 2000);
+//         //     return;
+//         // }
+//         // if (employee){
+//         //     console.log("you entered: " + employee);
+//         // } else {
+//         //     console.log("Nothing entered!");
+//         //     alert("please enter something ");
+//         //     return;
+//         // }
+//         const employee_element = createElWithClass("div", "employee");
+//         const employee_content = createElWithClass("div", "content");
+//         const employee_input = <HTMLInputElement> createElWithClass("input", "text");
+//         employee_input.type = "text";
+//         employee_input.value = employee;
+//         employee_input.setAttribute("readonly", "readonly");
+//         const employee_action = createElWithClass("div", "actions");
+//         const employee_edit = createElWithClass("button", "edit");
+//         employee_edit.innerHTML = "Edit";
+//         const employee_delete = createElWithClass("button", "delete");
+//         employee_delete.innerHTML = "Delete";
+//         employee_content.appendChild(employee_input);
+//         employee_action.appendChild(employee_edit);
+//         employee_action.appendChild(employee_delete);
+//         employee_element.appendChild(employee_content);
+//         employee_element.appendChild(employee_action);
+//         if (!list_element) {
+//             console.log("cannot found list element");
+//             return;
+//         }
+//         list_element.appendChild(employee_element);
+//         input.value = "";       // clean the value after append
+//         employee_edit.addEventListener("click", ()=>{
+//             if (employee_input.readOnly) {
+//                 employee_input.removeAttribute("readonly");
+//                 employee_input.focus();
+//                 employee_edit.innerText = "Save";
+//             } else {
+//                 console.log ("Save");
+//                 employee_input.setAttribute("readonly", "readonly");
+//                 employee_edit.innerText = "Edit";
+//             }
+//         })
+//         employee_delete.addEventListener("click", () => {
+//             if(confirm("Are you sure to kick this person out?")){
+//                 list_element.removeChild(employee_element);
+//             }
+//         })
+//     })
+// })
--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -3,25 +3,46 @@ const form : HTMLElement | null = document.querySelector("#new-employee-form"); 
 const input = document.querySelector("#new-employee-input") as HTMLInputElement;
 const list_element = document.querySelector("#employees");
 const header = document.querySelector("header");
-const submit_button = document.querySelector("#new-employee-submit") as HTMLButtonElement;
-const todoList = document.querySelector(".employee");
-
+const dropdowns = document.querySelectorAll<HTMLElement>(".dropdown");
 
 // ----- Event Listeners -----
-// submit_button.addEventListener('click', addName);
-if (!form) {
-    console.log("cannot found form");
-} else {
-    form.addEventListener("submit", addNew);
-}
-
-if (!list_element) {
-    console.log("cannot found list element");
-} else {
-    list_element.addEventListener("click", check);
+if (form) { 
+    form.addEventListener("submit", addNew); 
 }
 
 
+if (list_element) {
+    list_element.addEventListener("click", editNDelete);
+}
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector(".select")as HTMLElement;
+    const icon = dropdown.querySelector(".icon")as HTMLElement;
+    const menu = dropdown.querySelector(".menu") as HTMLElement;
+    const options = dropdown.querySelectorAll<HTMLElement>(".menu li");
+    const selected = dropdown.querySelector(".selected") as HTMLElement;
+
+    select.addEventListener("click", () => {
+        select.classList.toggle("select-clicked");  //add clicked select style to the select element
+        icon.classList.toggle("icon-rotate");      // add rotate style to icon
+        menu.classList.toggle("menu-open");        // add open style to menu
+    });
+
+    options.forEach(option => {
+        option.addEventListener("click",()=>{
+            selected.innerText = option.innerText;      // change the inner text to clicked option's inner text
+            select.classList.remove("select-clicked");  // remove the clicked select style
+            select.classList.remove("icon-rotate");     // remove rotate style to icon
+            menu.classList.remove("menu-open");         // remove open style to the menu
+            
+            options.forEach(option => {
+                option.classList.remove("active");      // remove active class from all options
+            });
+
+            option.classList.add("active");             // add active style to this clicked option
+        });
+    });
+})
 
 
 // ----- Functions -----
@@ -32,6 +53,7 @@ function createElWithClass(newEl:string, newClass:string) {
     current.classList.add(newClass)
     return current
 }
+
 
 function addNew(e: Event) {
     e.preventDefault();         // stop from refresh the page
@@ -81,178 +103,152 @@ function addNew(e: Event) {
     employee_element.appendChild(employee_content);
     employee_element.appendChild(employee_action);
     
-    
-
     if (!list_element) {
         console.log("cannot found list element");
         return;
+    } else {
+        list_element.appendChild(employee_element);
     }
 
-    list_element.appendChild(employee_element);
-    
     input.value = "";       // clean the value after append
+}
 
-    employee_edit.addEventListener("click", ()=>{
+
+// edit employee name or delete the employee
+function editNDelete(e:Event){
+    console.log(e.target);
+    const item = <HTMLElement> e.target;
+    const employee_action = <HTMLElement>item.parentElement;
+    const employee_element = <HTMLElement>employee_action.parentElement;
+
+    if (item.classList[0] === "delete") {  
+        if(confirm("Are you sure to kick this person out?")){
+            employee_element.classList.toggle("employee-fall");      // add animation
+            
+            employee_element.addEventListener("transitionend", function(){
+                employee_element.remove();      // remove after transition end
+            })
+            
+        }
+    }
+
+    if (item.classList[0] === "edit") {  
+        const employee_input = <HTMLInputElement>employee_element.querySelector(".text");
         if (employee_input.readOnly) {
             employee_input.removeAttribute("readonly");
             employee_input.focus();
-            employee_edit.innerText = "Save";
+            item.innerText = "Save";
         } else {
             console.log ("Save");
             employee_input.setAttribute("readonly", "readonly");
-            employee_edit.innerText = "Edit";
+            item.innerText = "Edit";
         }
-
-    })
-
-    employee_delete.addEventListener("click", () => {
-        if(confirm("Are you sure to kick this person out?")){
-            list_element.removeChild(employee_element);
-        }
-    })
+    }
 }
 
-// function addName(event:Event) {
-//     event.preventDefault();
-//     // console.log("click");
-
-//     const todo_div = createElWithClass("div", "employee");
-
-//     const new_todo = createElWithClass("li", "content");
-//     new_todo.innerText = 'hey';
-
-//     todo_div.appendChild(new_todo);
-
-//     const employee_edit = createElWithClass("button", "edit");
-//     employee_edit.innerHTML = "Edit";
-
-//     const employee_delete = createElWithClass("button", "delete");
-//     employee_delete.innerHTML = "Delete";
-
-//     todo_div.appendChild(employee_edit);
-//     todo_div.appendChild(employee_delete);
-
-//     if (!list_element) {
-//         console.log("cannot found list element");
+// window.addEventListener("load", () => {
+    
+//     if (!form) {
+//         console.log("cannot found form");
 //         return;
 //     }
-        
-//     list_element.appendChild(todo_div);
 
-// }
+//     form.addEventListener("submit", (e) => {
+//         e.preventDefault();         // stop from refresh the page
 
-function check(e:Event){
-    console.log(e.target);
-    const item = <HTMLElement> e.target;
-    if (item.classList[0] === "delete") {
-        const employee_element = <HTMLElement>item.parentElement;
-        list_element.removeChild(employee_element);
-    }
-}
+//         // console.log("submit success");
+//         if (!input) {
+//             console.log("cannot found input");
+//             // alert("please enter something ");
+//             return;
+//         }
 
-window.addEventListener("load", () => {
-    
-    if (!form) {
-        console.log("cannot found form");
-        return;
-    }
+//         const employee : string | null = input.value;
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();         // stop from refresh the page
-
-        // console.log("submit success");
-        if (!input) {
-            console.log("cannot found input");
-            // alert("please enter something ");
-            return;
-        }
-
-        const employee : string | null = input.value;
-
-        // console.log("you entered: " + employee);
+//         // console.log("you entered: " + employee);
 
         
-        if (!header) {
-            console.log("cannot found header");
-            return;
-        }
+//         if (!header) {
+//             console.log("cannot found header");
+//             return;
+//         }
 
-        if (!employee) {
-            const incorrectSub = document.createElement("p")
-            incorrectSub.textContent = "Please insert the employee name"
-            incorrectSub.style.color = "#E74C3C";
-            header.appendChild(incorrectSub);
+//         // if (!employee) {
+//         //     const incorrectSub = document.createElement("p")
+//         //     incorrectSub.textContent = "Please insert the employee name"
+//         //     incorrectSub.style.color = "#E74C3C";
+//         //     header.appendChild(incorrectSub);
 
-            // disappear after 2s
-            setTimeout(function(){
-               header.removeChild(incorrectSub);
-            }, 2000);
+//         //     // disappear after 2s
+//         //     setTimeout(function(){
+//         //        header.removeChild(incorrectSub);
+//         //     }, 2000);
             
-            return;
-        }
+//         //     return;
+//         // }
 
-        // if (employee){
-        //     console.log("you entered: " + employee);
-        // } else {
-        //     console.log("Nothing entered!");
-        //     alert("please enter something ");
-        //     return;
-        // }
+//         // if (employee){
+//         //     console.log("you entered: " + employee);
+//         // } else {
+//         //     console.log("Nothing entered!");
+//         //     alert("please enter something ");
+//         //     return;
+//         // }
 
-        const employee_element = createElWithClass("div", "employee");
+//         const employee_element = createElWithClass("div", "employee");
 
-        const employee_content = createElWithClass("div", "content");
+//         const employee_content = createElWithClass("div", "content");
 
-        const employee_input = <HTMLInputElement> createElWithClass("input", "text");
-        employee_input.type = "text";
-        employee_input.value = employee;
-        employee_input.setAttribute("readonly", "readonly");
+//         const employee_input = <HTMLInputElement> createElWithClass("input", "text");
+//         employee_input.type = "text";
+//         employee_input.value = employee;
+//         employee_input.setAttribute("readonly", "readonly");
 
-        const employee_action = createElWithClass("div", "actions");
+//         const employee_action = createElWithClass("div", "actions");
 
-        const employee_edit = createElWithClass("button", "edit");
-        employee_edit.innerHTML = "Edit";
+//         const employee_edit = createElWithClass("button", "edit");
+//         employee_edit.innerHTML = "Edit";
 
-        const employee_delete = createElWithClass("button", "delete");
-        employee_delete.innerHTML = "Delete";
+//         const employee_delete = createElWithClass("button", "delete");
+//         employee_delete.innerHTML = "Delete";
 
-        employee_content.appendChild(employee_input);
+//         employee_content.appendChild(employee_input);
 
-        employee_action.appendChild(employee_edit);
-        employee_action.appendChild(employee_delete);
+//         employee_action.appendChild(employee_edit);
+//         employee_action.appendChild(employee_delete);
         
-        employee_element.appendChild(employee_content);
-        employee_element.appendChild(employee_action);
+//         employee_element.appendChild(employee_content);
+//         employee_element.appendChild(employee_action);
         
         
 
-        if (!list_element) {
-            console.log("cannot found list element");
-            return;
-        }
+//         if (!list_element) {
+//             console.log("cannot found list element");
+//             return;
+//         }
 
-        list_element.appendChild(employee_element);
+//         list_element.appendChild(employee_element);
         
-        input.value = "";       // clean the value after append
+//         input.value = "";       // clean the value after append
 
-        employee_edit.addEventListener("click", ()=>{
-            if (employee_input.readOnly) {
-                employee_input.removeAttribute("readonly");
-                employee_input.focus();
-                employee_edit.innerText = "Save";
-            } else {
-                console.log ("Save");
-                employee_input.setAttribute("readonly", "readonly");
-                employee_edit.innerText = "Edit";
-            }
+//         employee_edit.addEventListener("click", ()=>{
+//             if (employee_input.readOnly) {
+//                 employee_input.removeAttribute("readonly");
+//                 employee_input.focus();
+//                 employee_edit.innerText = "Save";
+//             } else {
+//                 console.log ("Save");
+//                 employee_input.setAttribute("readonly", "readonly");
+//                 employee_edit.innerText = "Edit";
+//             }
 
-        })
+//         })
 
-        employee_delete.addEventListener("click", () => {
-            if(confirm("Are you sure to kick this person out?")){
-                list_element.removeChild(employee_element);
-            }
-        })
-    })
+//         employee_delete.addEventListener("click", () => {
+//             if(confirm("Are you sure to kick this person out?")){
+//                 list_element.removeChild(employee_element);
+//             }
+//         })
+//     })
     
-})
+// })
